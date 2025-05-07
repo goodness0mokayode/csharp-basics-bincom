2870979a07c6938a51c4c5496076084ec0ace1cc
Goodness <goodnessomokayode@gmail.com>
2025-05-08 00:34:39 +0100

    Hello World .NET

---
+++ 1HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1HelloWorld
+{
+    class HW
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
