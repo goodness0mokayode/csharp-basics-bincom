c0d5ba62e8bba76d74fba95ed9fb0170a3c3fccf
Goodness <goodnessomokayode@gmail.com>
2025-05-12 17:23:03 +0100

    Add BootStrap card and Centred Box with Custom TypeWriter slidedown and slide in CSS animations; add buttons to card which connect to CV and Projects Controller

--- PortfolioWithBincom/obj/Release/net9.0/PortfolioWithBincom.AssemblyInfo.cs
+++ PortfolioWithBincom/obj/Release/net9.0/PortfolioWithBincom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWithBincom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744211df2d229c6813278c6c953cd404fb51a0bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2deef67967056f6ca62e716a6a7ff58ccc814181")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWithBincom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWithBincom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
